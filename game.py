94ed3f037d9e8ceebd0a0081fbb36d201a093e51
Pedro Lima <up202108806@up.pt>
2024-03-03 18:07:31 +0000

    Fixing More Bugs

--- game.py
+++ game.py
@@ -1,13 +1,29 @@
-import state
+from state import State
+from board import PlayerEnum
 
 class Game:
     def __init__(self):
-        self.board = Board()
-        self.player = PlayerEnum.BLACK
-    
-    def change_player(self):
-        self.player = opponent_player(self.player)
+        self.state = State()
 
-    def play(self):
-        pass
+def play_simulation(initial_state: State):
+    initial_state.draw()
 
+    for i in range(20):
+        # print("Available moves: ", initial_state.board.get_available_moves(initial_state.player))
+        move_to_exe = initial_state.get_available_moves()[0]
+        print()
+        print("Move to execute: ", move_to_exe)
+
+        print()
+        print('-'*50)
+        print()
+
+        initial_state.execute_move(move_to_exe)
+        initial_state.draw()
+        if initial_state.check_winner() != PlayerEnum.EMPTY:
+            print("Winner: ", initial_state.check_winner())
+            break
+
+if __name__ == '__main__':
+    g = Game()
+    play_simulation(g.state)

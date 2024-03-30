d6dfe797a70cee02487dbf445c1ef94cb57932fd
Félix Martins <felix.marcial@hotmail.com>
2024-03-30 15:36:30 +0000

    Changed sys path appending to use os

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 import numpy as np
 from copy import deepcopy

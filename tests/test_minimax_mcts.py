b32621c9af51a9b7873a9224529ff15f6de273f7
Félix Martins <felix.marcial@hotmail.com>
2024-04-01 09:26:41 +0100

    Completed tests for minimax vs MCTS (quick/better)

--- tests/test_minimax_mcts.py
+++ tests/test_minimax_mcts.py
@@ -1,6 +1,6 @@
 import sys
 import os
-    
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
@@ -22,20 +22,8 @@ from heuristics.approximate_enemy_heuristic import ApproximateEnemyHeuristic
 from minimax import execute_minimax_move, get_minimax_move
 from monte_carlo_tree_search.tree import MonteCarloTree
 from monte_carlo_tree_search.tree_heuristics import MonteCarloTreeHeuristic
-import time
 
-"""
-Minimax: state -> move
-move -> state (execute_move)
 
-MCTS:
-(state)
-move -> move
-move -> state (execute_move)
-
-"""
-
-@test
 def test_mcts_vs_minimax(
     minimax,
     mcts: MonteCarloTree,
@@ -44,103 +32,139 @@ def test_mcts_vs_minimax(
     mcts_limit: int,
     mcts_is_white: bool,
     nr: int,
-    log_states=True,
+    log_states=False,
 ):
     wins = {}
     time_minimax = 0
     time_mcts = 0
 
+    def ai_white(state: State) -> State:
+        if mcts_is_white:
+            mcts.train_until(mcts_limit)
+            move = mcts.get_best_move()
+            mcts.update_move(move)
+            return state.execute_move(move)
+        move = minimax(state)
+        mcts.update_move(move)
+        return state.execute_move(move)
+
+    def ai_black(state: State) -> State:
+        if mcts_is_white:
+            move = minimax(state)
+            mcts.update_move(move)
+            return state.execute_move(move)
+
+        mcts.train_until(mcts_limit)
+        move = mcts.get_best_move()
+        mcts.update_move(move)
+        return state.execute_move(move)
+
     for _ in range(nr):
         state = State(width, height)
-        mcts = MonteCarloTree(width, height, 2, 10)  # white: 2, 10; black: 10, 2
-        # try:
-        while not state.game_over():
-            if log_states:
-                state.draw()
-            if state.is_white_turn():
-                if mcts_is_white:
-                    start = time.time()
-
-                    mcts.train_until(mcts_limit)
-                    move = mcts.get_best_move()
-                    mcts.update_move(move)
-                    state = state.execute_move(move)
-
-                    end = time.time()
-                    time_mcts += end - start
-                else:
-                    start = time.time()
-
-                    move = minimax(state)
-                    state = state.execute_move(move)
-                    mcts.update_move(move)
-
-                    end = time.time()
-                    time_minimax += end - start
-            else:
-                if mcts_is_white:
-                    start = time.time()
-
-                    move = minimax(state)
-                    state = state.execute_move(move)
-                    mcts.update_move(move)
-
-                    end = time.time()
-                    time_minimax += end - start
-                else:
-                    start = time.time()
-
-                    mcts.train_until(mcts_limit)
-                    move = mcts.get_best_move()
-                    mcts.update_move(move)
-                    state = state.execute_move(move)
-
-                    end = time.time()
-                    time_mcts += end - start
-
-        if state.count == DRAW_COUNTER_THRESHOLD:
-            # if X moves have passed without a capture, the game is a draw
-            print(f"Draw by {DRAW_COUNTER_THRESHOLD} moves rule (no captures)")
-
-        if log_states:
-            state.draw()
-
-        winner = state.check_winner()
-
-        if log_states:
-            print(f"Winner: {winner}")
+        winner, time_white, time_black = state.run_ais(ai_white, ai_black, log_states)
 
         wins[winner] = wins.get(winner, 0) + 1
-        # except Exception as e:
-        #     print(e)
-        #     state.draw()
-        #     print(state.get_available_moves())
-        #     raise e
-    return wins
+        if mcts_is_white:
+            time_mcts += time_white
+            time_minimax += time_black
+        else:
+            time_minimax += time_white
+            time_mcts += time_black
+        mcts.reset_game()
+
+    return wins, time_minimax / nr, time_mcts / nr
 
 
-def test_nr_pieces_vs_mcts_quick():
-    minimax = get_minimax_move(NrPiecesHeuristic().evaluate_board, 2)
+def minimax_vs_mcts_wrapper(minimax, nr: int, mcts_iters: int):
     width, height = 9, 5
-    mcts = MonteCarloTree(width, height, 2, 10)  # white: 2, 10; black: 10, 2
-    nr = 100
-    test_mcts_vs_minimax(
+    mcts = MonteCarloTree.from_player(width, height, Player.WHITE)
+
+    wins, time_minimax, time_mcts = test_mcts_vs_minimax(
         minimax,
         mcts,
         nr=nr,
         mcts_is_white=True,
-        mcts_limit=100,  # mcts quick
+        mcts_limit=mcts_iters,
         width=width,
         height=height,
     )
-    mcts = MonteCarloTree(width, height, 10, 2)  # white: 2, 10; black: 10, 2
-    test_mcts_vs_minimax(
+    print("White: mcts, Black: minimax")
+    print(f"{wins}, minimax: {time_minimax}, mcts: {time_mcts}")
+
+    mcts = MonteCarloTree.from_player(width, height, Player.BLACK)
+
+    wins, time_minimax, time_mcts = test_mcts_vs_minimax(
         minimax,
         mcts,
         nr=nr,
         mcts_is_white=False,
-        mcts_limit=100,  # mcts quick
+        mcts_limit=mcts_iters,
         width=width,
         height=height,
     )
+    print("White: minimax, Black: mcts")
+    print(f"{wins}, minimax: {time_minimax}, mcts: {time_mcts}")
 
+
+@test
+def test_nr_pieces_vs_mcts(nr: int, mcts_iters: int, minimax_depth: int):
+    minimax = get_minimax_move(NrPiecesHeuristic().evaluate_board, minimax_depth)
+    minimax_vs_mcts_wrapper(minimax, nr, mcts_iters)
+
+
+@test
+def test_nr_pieces_approx_enemy_vs_mcts(nr: int, mcts_iters: int, minimax_depth: int):
+    minimax = get_minimax_move(
+        HeuristicsList(
+            np.array([NrPiecesHeuristic(), ApproximateEnemyHeuristic()]),
+            np.array([2, 1]),
+        ).evaluate_board,
+        minimax_depth,
+    )
+    minimax_vs_mcts_wrapper(minimax, nr, mcts_iters)
+
+
+@test
+def test_pieces_adjacent_groups_vs_mcts(nr: int, mcts_iters: int, minimax_depth: int):
+    minimax = get_minimax_move(
+        HeuristicsList(
+            np.array(
+                [NrPiecesHeuristic(), AdjacentPiecesHeuristic(), GroupsHeuristic()]
+            ),
+            np.array([10, 2, 1]),
+        ).evaluate_board,
+        minimax_depth,
+    )
+    minimax_vs_mcts_wrapper(minimax, nr, mcts_iters)
+
+
+@test
+def test_pieces_groups_center_vs_mcts(nr: int, mcts_iters: int, minimax_depth: int):
+    minimax = get_minimax_move(
+        HeuristicsList(
+            np.array(
+                [NrPiecesHeuristic(), GroupsHeuristic(), CenterControlHeuristic()]
+            ),
+            np.array([10, 1, 1]),
+        ).evaluate_board,
+        minimax_depth,
+    )
+    minimax_vs_mcts_wrapper(minimax, nr, mcts_iters)
+
+
+if __name__ == "__main__":
+    nr = 20
+    mcts_quick_iters, mcts_better_iters = 100, 1000
+    minimax_lower_depth, minimax_higher_depth = 2, 4
+
+    test_nr_pieces_vs_mcts(nr, mcts_quick_iters, minimax_lower_depth)
+    test_nr_pieces_vs_mcts(nr, mcts_better_iters, minimax_higher_depth)
+
+    test_nr_pieces_approx_enemy_vs_mcts(nr, mcts_quick_iters, minimax_lower_depth)
+    test_nr_pieces_approx_enemy_vs_mcts(nr, mcts_better_iters, minimax_higher_depth)
+
+    test_pieces_adjacent_groups_vs_mcts(nr, mcts_quick_iters, minimax_lower_depth)
+    test_pieces_adjacent_groups_vs_mcts(nr, mcts_better_iters, minimax_higher_depth)
+
+    test_pieces_groups_center_vs_mcts(nr, mcts_quick_iters, minimax_lower_depth)
+    test_pieces_groups_center_vs_mcts(nr, mcts_better_iters, minimax_higher_depth)
